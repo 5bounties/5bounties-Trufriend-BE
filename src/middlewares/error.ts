b87c032fd194c7630a7fa8221abf59b2fece6329
ahargunyllib <billy.bpm03@gmail.com>
2024-05-06 08:53:51 +0700

    fix: refactor error handling and logging middleware

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -21,9 +21,11 @@ export const errorHandlerEndpoint = (
 	res.locals.errorMessage = error.message;
 
 	if (error instanceof ValidationError) {
+		res.locals.errorMessage = error.details;
+
 		res.status(httpStatus.BAD_REQUEST).send({
 			status: httpStatus.BAD_REQUEST,
-			message: error.details,
+			message: res.locals.errorMessage,
 			data: [],
 		});
 	} else if (error instanceof PrismaClientKnownRequestError) {
@@ -31,36 +33,44 @@ export const errorHandlerEndpoint = (
 		switch (error.code) {
 			case "P2025": {
 				// P2025: Record not found
+				res.locals.errorMessage = `Record not found on the ${error.meta?.target}`;
+
 				res.status(httpStatus.NOT_FOUND).send({
 					status: httpStatus.NOT_FOUND,
-					message: `Record not found on the ${error.meta?.target}`,
+					message: res.locals.errorMessage,
 					data: [],
 				});
 				break;
 			}
 			case "P2023": {
 				// P2023: The provided value for the column is too long for the column's type
+				res.locals.errorMessage = `The provided value for the column is too long for the column's type on the ${error.meta?.target}`;
+
 				res.status(httpStatus.BAD_REQUEST).send({
 					status: httpStatus.BAD_REQUEST,
-					message: `The provided value for the column is too long for the column's type on the ${error.meta?.target}`,
+					message: res.locals.errorMessage,
 					data: [],
 				});
 				break;
 			}
 			case "P2016": {
 				// P2016: The provided value for the column is too short for the column's type
+				res.locals.errorMessage = `The provided value for the column is too short for the column's type on the ${error.meta?.target}`;
+
 				res.status(httpStatus.BAD_REQUEST).send({
 					status: httpStatus.BAD_REQUEST,
-					message: `The provided value for the column is too short for the column's type on the ${error.meta?.target}`,
+					message: res.locals.errorMessage,
 					data: [],
 				});
 				break;
 			}
 			case "P2002": {
 				// P2022: Unique constraint failed
+				res.locals.errorMessage = `Unique constraint failed on the ${error.meta?.target}`;
+
 				res.status(httpStatus.BAD_REQUEST).send({
 					status: httpStatus.BAD_REQUEST,
-					message: `Unique constraint failed on the ${error.meta?.target}`,
+					message: res.locals.errorMessage,
 					data: [],
 				});
 				break;
@@ -69,15 +79,17 @@ export const errorHandlerEndpoint = (
 				res.status(httpStatus.INTERNAL_SERVER_ERROR).send({
 					status: httpStatus.INTERNAL_SERVER_ERROR,
 					message: httpStatus[httpStatus.INTERNAL_SERVER_ERROR],
-					data: error,
+					data: error.message,
 				});
 				break;
 			}
 		}
 	} else if (error instanceof ApiError) {
+		res.locals.errorMessage = error.message;
+
 		res.status(error.statusCode).send({
 			status: error.statusCode,
-			message: error.message,
+			message: res.locals.errorMessage,
 			data: [],
 		});
 	} else {
